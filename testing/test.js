6f81f0b5cece677573dc6ad887fd86d7f93531c1
ASHUTOSH KUMAR JHA <84619413+ASHUdev05@users.noreply.github.com>
2021-12-26 10:00:04 +0530

    The testing files
    (:

---
+++ testing/test.js
@@ -0,0 +1,17 @@
+const discord = require('discord.js')
+const client = new discord.Client({
+    intents: [discord.Intents.FLAGS.GUILDS, discord.Intents.FLAGS.GUILD_MESSAGES]
+})
+
+const { InfoRequester } = require('./target/main') //ok we have js one running in mainstream i will handle this personally if i find a fix
+ // but if we only import one then ppl only can import one // i prefer having it in js that making 3 packages, ik, xd, imagine 3 packages // ok
+const infoRequester = new InfoRequester(client,'works') // im testing if works outside the folder
+ 
+client.on('ready', async() => { // bro you literally required ./main how will it work? //thats when i got the files out of the folder
+    console.log('Noice') 
+
+    const info = await infoRequester.getBotInfo('876901696738058261') 
+    console.log(info) 
+})    
+
+client.login('ODU1MTA0OTg1NTk1Mzc5NzUy.YMtokw.m-31K6NojuR8q9H7Pw_eY4f3Fv0') 
